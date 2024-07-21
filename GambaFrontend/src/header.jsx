fda03ffc48823a4ed95713df702cf4b1f163f361
tadyg8 <78745124+tadyg8@users.noreply.github.com>
2024-07-21 15:55:54 +0200

    Moved color changing js script to its own file, added a js dir

--- GambaFrontend/src/header.jsx
+++ GambaFrontend/src/header.jsx
@@ -2,19 +2,12 @@ import './css/header.css'
 import Hamburger from 'hamburger-react'
 import Search from './search.jsx'
 import HeaderLoginButton from './headerLoginButton.jsx'
+import  rootBackgroundColorChanger from './js/rootBackgroundColorChanger.js'
 import { useLayoutEffect } from 'react'
 
 export default function Header() {
 
-    var root = document.querySelector(':root');
-    var cs = getComputedStyle(root);
-    var rgba = cs.getPropertyValue('--color-secondary');
-
-    const rgbaValues = rgba.slice(5, -1).split(',').map(Number);
-    const [r, g, b] = rgbaValues;
-
-    root.style.setProperty('--color-secondary-darker', RGBToHSL1(r, g, b));
-    root.style.setProperty('--color-primary-darker', RGBToHSL2(r, g, b));
+    rootBackgroundColorChanger();
 
     return (
         <header>
@@ -32,97 +25,3 @@ export default function Header() {
         </header>
     )
 }
-
-function RGBToHSL1(r,g,b) {
-    // Make r, g, and b fractions of 1
-    r /= 255;
-    g /= 255;
-    b /= 255;
-  
-    // Find greatest and smallest channel values
-    let cmin = Math.min(r,g,b),
-        cmax = Math.max(r,g,b),
-        delta = cmax - cmin,
-        h = 0,
-        s = 0,
-        l = 0;
-    
-    // Calculate hue
-    // No difference
-    if (delta == 0)
-      h = 0;
-    // Red is max
-    else if (cmax == r)
-      h = ((g - b) / delta) % 6;
-    // Green is max
-    else if (cmax == g)
-      h = (b - r) / delta + 2;
-    // Blue is max
-    else
-      h = (r - g) / delta + 4;
-  
-    h = Math.round(h * 60);
-      
-    // Make negative hues positive behind 360°
-    if (h < 0)
-        h += 360;
-    
-    // Calculate lightness
-    l = (cmax + cmin) / 2;
-  
-    // Calculate saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-      
-    // Multiply l and s by 100
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-  
-    return "hsl(" + h + "," + (s/1) + "%," + (l/8) + "%)";
-  }
-  
-  function RGBToHSL2(r,g,b) {
-    // Make r, g, and b fractions of 1
-    r /= 255;
-    g /= 255;
-    b /= 255;
-  
-    // Find greatest and smallest channel values
-    let cmin = Math.min(r,g,b),
-        cmax = Math.max(r,g,b),
-        delta = cmax - cmin,
-        h = 0,
-        s = 0,
-        l = 0;
-    
-    // Calculate hue
-    // No difference
-    if (delta == 0)
-      h = 0;
-    // Red is max
-    else if (cmax == r)
-      h = ((g - b) / delta) % 6;
-    // Green is max
-    else if (cmax == g)
-      h = (b - r) / delta + 2;
-    // Blue is max
-    else
-      h = (r - g) / delta + 4;
-  
-    h = Math.round(h * 60);
-      
-    // Make negative hues positive behind 360°
-    if (h < 0)
-        h += 360;
-    
-    // Calculate lightness
-    l = (cmax + cmin) / 2;
-  
-    // Calculate saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-      
-    // Multiply l and s by 100
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-  
-    return "hsl(" + h + "," + (s/1) + "%," + (l/25) + "%)";
-  }
