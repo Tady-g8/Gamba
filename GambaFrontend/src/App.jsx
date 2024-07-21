438189ebca079acb7c0ba786dec7d626cd3b9f46
tadyg8 <78745124+tadyg8@users.noreply.github.com>
2024-07-21 15:51:17 +0200

    Moved background color changing functions to header

--- GambaFrontend/src/App.jsx
+++ GambaFrontend/src/App.jsx
@@ -1,119 +1,12 @@
 import { useState } from 'react'
 import Header from './header.jsx'
 
-function App() {
-
-  var root = document.querySelector(':root');
-  var cs = getComputedStyle(root);
-  var rgba = cs.getPropertyValue('--color-secondary');
-
-  const rgbaValues = rgba.slice(5, -1).split(',').map(Number);
-  const [r, g, b] = rgbaValues;
-
-  root.style.setProperty('--color-secondary-darker', RGBToHSL1(r, g, b));
-  root.style.setProperty('--color-primary-darker', RGBToHSL2(r, g, b));
-
+export default function App() {
   return (
     <>
       <Header />
     </>
   )
-}
-
-export default App
-
-function RGBToHSL1(r,g,b) {
-  // Make r, g, and b fractions of 1
-  r /= 255;
-  g /= 255;
-  b /= 255;
-
-  // Find greatest and smallest channel values
-  let cmin = Math.min(r,g,b),
-      cmax = Math.max(r,g,b),
-      delta = cmax - cmin,
-      h = 0,
-      s = 0,
-      l = 0;
-  
-  // Calculate hue
-  // No difference
-  if (delta == 0)
-    h = 0;
-  // Red is max
-  else if (cmax == r)
-    h = ((g - b) / delta) % 6;
-  // Green is max
-  else if (cmax == g)
-    h = (b - r) / delta + 2;
-  // Blue is max
-  else
-    h = (r - g) / delta + 4;
-
-  h = Math.round(h * 60);
-    
-  // Make negative hues positive behind 360°
-  if (h < 0)
-      h += 360;
-  
-  // Calculate lightness
-  l = (cmax + cmin) / 2;
-
-  // Calculate saturation
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    
-  // Multiply l and s by 100
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
-
-  return "hsl(" + h + "," + (s/1) + "%," + (l/8) + "%)";
-}
-
-function RGBToHSL2(r,g,b) {
-  // Make r, g, and b fractions of 1
-  r /= 255;
-  g /= 255;
-  b /= 255;
-
-  // Find greatest and smallest channel values
-  let cmin = Math.min(r,g,b),
-      cmax = Math.max(r,g,b),
-      delta = cmax - cmin,
-      h = 0,
-      s = 0,
-      l = 0;
-  
-  // Calculate hue
-  // No difference
-  if (delta == 0)
-    h = 0;
-  // Red is max
-  else if (cmax == r)
-    h = ((g - b) / delta) % 6;
-  // Green is max
-  else if (cmax == g)
-    h = (b - r) / delta + 2;
-  // Blue is max
-  else
-    h = (r - g) / delta + 4;
-
-  h = Math.round(h * 60);
-    
-  // Make negative hues positive behind 360°
-  if (h < 0)
-      h += 360;
-  
-  // Calculate lightness
-  l = (cmax + cmin) / 2;
-
-  // Calculate saturation
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    
-  // Multiply l and s by 100
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
-
-  return "hsl(" + h + "," + (s/1) + "%," + (l/25) + "%)";
 }
 
 //money division number 3.9176470588235294
